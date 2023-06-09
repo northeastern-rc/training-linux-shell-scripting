0113073fcef891a212133874b482948afe944d96
phaltankarvrushali <44588168+phaltankarvrushali@users.noreply.github.com>
2023-06-09 16:03:22 -0400

    Update divide_and_split_files.bash

--- Exercise_1/divide_and_split_files.bash
+++ Exercise_1/divide_and_split_files.bash
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+####Use during training####
+#SBATCH --partition=reservation
+#SBATCH --reservation=bootcamp_cpu_2023
+## Reservation bootcamp_cpu_2023 will not be available after bootcamp
+#######################################
+####Use after training####
 #SBATCH --partition=express
+#######################################
+
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --job-name=divide_and_split_files
